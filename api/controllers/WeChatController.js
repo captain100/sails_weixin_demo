cfddefdd0b5b41d93c0d62c75f77bdf2db9cc8d8
qiushi <qiushi@qiushideMBP.lan>
2015-12-25 18:55:22 +0800

    ddsds

--- api/controllers/WeChatController.js
+++ api/controllers/WeChatController.js
@@ -8,31 +8,7 @@ var wechat_config = {
 
 module.exports = {
     'wechat': function (req, res, next) {
-        console.log('------------------')
-        console.log(req);
-        wechat(wechat_config, function (req, res, next) {
-            console.log(11111111111)
-            // 微信输入信息都在req.weixin上
-            var message = req.weixin;
-            console.log(message);
-            if (message.Content === 'qiushi') {
-                // 回复qiushi(普通回复)
-                res.reply('hehe');
-
-            } else if (message.Event === 'CLICK' && message.EventKey === 'CREAT_TASK_1') {
-                var openid = message.FromUserName;
-                request('http://www.cpzero.cn/createTask?openid=' + openid, function (error, response, info) {
-
-                })
-                next();
-
-            } else {
-                res.reply({
-                    content: '欢迎你加入由XX公司提供的XX试验。在此之前请确认你是否已经在你的主治医师的指导下签署书面合同已经签署请\n回复：是\n否则请联系你的主治医师',
-                    type: 'text'
-                });
-            }
-        })(req, res, next);
+        console.log('---------23213-------');
     }
 
 }
