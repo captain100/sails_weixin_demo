106bba23260c22113c26fd136b4ff73364411aef
qiushi <qiushi@qiushideMBP.lan>
2015-12-31 18:01:57 +0800

    update

--- config/policies.js
+++ config/policies.js
@@ -1,10 +1,3 @@
-var request = require('request');
-var wechat = require('wechat');
-var wechat_config = {
-    token: 'nj8sJ8YyNuxJCc',//开发者 token
-    appid: 'wxb4fb29266130bb85',// appid
-    encodingAESKey: 'BahC4uNa3dY6wo5U3mRpVf4yxkQtXs6OyDXEe2GudmR'//encodingAESKey
-};
 
 module.exports.policies = {
     '*': true,
